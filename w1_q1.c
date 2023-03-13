d8c12f5f610bf32f1470772e1c1ddb255b4f3f0c
Aman0023 <126680196+Aman0023@users.noreply.github.com>
2023-03-13 12:54:53 +0530

    week 1 assignment submitted

--- w1_q1.c
+++ w1_q1.c
@@ -4,13 +4,17 @@ int main()
     int d;
     for(d=1;d<=100;d++)
     {
-        if(d%3==0)
+        if(d%3==0 & d%5==0)
         {
-            printf("FIZZ\n");
+            printf("BUZZ FIZZ\n");
         }
         else if(d%5==0)
         {
             printf("BUZZ\n");
+        }
+        else if(d%3==0)
+        {
+            printf("FIZZ\n");
         }
         else
         {
